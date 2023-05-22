daaae2d74644fbc67f55fffbd1d676b7e569666d
HannachiHassen <hannachihassen07@gmail.com>
2023-05-22 11:21:02 -0400

    Update HomePageActions, LoginPageActions class

--- src/main/java/actions/HomePageActions.java
+++ src/main/java/actions/HomePageActions.java
@@ -1,8 +1,24 @@
 package actions;
 
+import org.openqa.selenium.support.PageFactory;
+
+import locators.HomePageLocators;
+import utils.HelperClass;
+
 public class HomePageActions {
 
-	public HomePageActions() {
-	}
+HomePageLocators homePageLocators = null;
+    
+    public HomePageActions() {
+         
+        this.homePageLocators = new HomePageLocators();
+ 
+        PageFactory.initElements(HelperClass.getDriver(),homePageLocators);
+    } 
+  
+    // Get the User name from Home Page
+    public String getHomePageText() {
+        return homePageLocators.homePageUserName.getText();
+    }
 
 }
